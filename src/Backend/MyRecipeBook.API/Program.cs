3961fdeb809b5144fba50fa3387e9fdef129caab
devfirmino <daanspfc@gmail.com>
2025-12-12 19:13:18 +0100

    Register scoped service in the dependency container

--- src/Backend/MyRecipeBook.API/Program.cs
+++ src/Backend/MyRecipeBook.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<>();
 
 var app = builder.Build();
 
